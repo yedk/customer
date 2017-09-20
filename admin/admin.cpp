60ca65347ff88d42f712e230eaeeaaa76ba9fbd6
admin <admin@DESKTOP-BGUORCN>
2017-09-20 12:49:11 +0800

    新加删除操作

--- admin/admin.cpp
+++ admin/admin.cpp
@@ -22,6 +22,7 @@ adminClass ::adminClass (QWidget *parent)
 	{
 		ui->textEdit->append(query.value(1).toString());
 	}
+	db.close();//¹Ø±ÕÊý¾Ý¿â
 }
 
 adminClass::~adminClass()
@@ -44,5 +45,28 @@ void adminClass::on_delete_dish_clicked()
 
 void adminClass::on_delete_customer_clicked()
 {
-
+	QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+	db.setDatabaseName("../rest.sql");
+	if (!db.open())
+	{
+		QMessageBox::critical(0, "Cannot open database",
+			"Unable to establish a database", QMessageBox::Cancel);
+	}
+	QSqlQuery query;
+	QString delete_sql;
+	QString name;
+	name = ui->customer_number->text();
+	delete_sql = "delete from customer where name = ?";
+	query.prepare(delete_sql);
+	query.addBindValue(name);
+	if (!query.exec())
+	{
+		QMessageBox::critical(0, "Cannot delete data",
+			"Cannot delete data", QMessageBox::Cancel);
+	}
+	else
+	{
+		QMessageBox::information(this, "information", "The number has been deleted!");
+	}
+	db.close();
 }
